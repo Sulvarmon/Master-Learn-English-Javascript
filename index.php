5c823462febbe27aab3a1d5ef631f7f9762892fc
Solvarmon <sh.mikolaichuki@gmail.com>
2022-08-15 20:58:13 +0400

    C

--- index.php
+++ index.php
@@ -33,7 +33,31 @@
 
 <body>
 
-    <div class="cont_s border_s">Heelooo</div>
+    <div class="website_bg_s"></div>
+
+     <div class="login_page">
+        <div class="login_cont_s m-auto_s mt-5_s text-white_s rounded_s p-4_s d-flex_s flex-column_s gap-2_s align-items-center_s">
+            <div class="h1_s">Log In Here</div>
+            <div class="login_username_input"><input class="p-2_s h5_s" type="text" placeholder="username"></div>
+            <div class="login_password_input"><input class="p-2_s h5_s" type="password" placeholder="password"></div>
+            <div class="login_show_hide_password"><input type="checkbox"><span class="ms-2">Show Password</span></div>
+            <button class="login_btn">Log In</button>
+            <div class="mt-5_s">Not Register ?</div>
+            <button class="register_btn">Register Here</button>
+        </div>
+    </div>
+
+    <div class="reg_page">
+        <div class="login_cont_s m-auto_s mt-5_s text-white_s rounded_s p-4_s d-flex_s flex-column_s gap-2_s align-items-center_s">
+            <div class="h1_s">Registration</div>
+            <div class="reg_username_input"><input class="p-2_s h5_s" type="text" placeholder="username"></div>
+            <div class="reg_password_input"><input class="p-2_s h5_s" type="password" placeholder="password"></div>
+            <div class="reg_show_hide_password"><input type="checkbox"><span class="ms-2">Show Password</span></div>
+            <button class="reg_btn">Register</button>
+            <div class="mt-5">Go Here To Log In</div>
+            <button class="login_here_btn ">Log In Here</button>
+        </div>
+    </div>
 
 </body>
 
